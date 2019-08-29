867f8d92fd204f597271af946cd5290d94f21118
EasonLin0716 <uu9924079@gmail.com>
2019-08-29 12:03:41 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// include express, handlebars, body-parser
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const loginCheck = require('./login_check')
+
+const users = require('./users.json')
+
+// Register an ifEquals helper for index to use
+// https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value
+const Handlebars = require("handlebars");
+Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const account = req.body
+  const result = loginCheck(account, users.results)
+  if (result.loginSuccess) {
+    res.render('login', { result })
+  } else {
+    res.render('index', { result })
+  }
+})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
